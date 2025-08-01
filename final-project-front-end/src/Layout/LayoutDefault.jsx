69313d9c0d696e7516b9c2bf9386d8d8b0dea5c5
AndreaCatapano <a.catapano96@gmail.com>
2025-08-01 22:20:46 +0200

    style: added css at home.jsx

--- final-project-front-end/src/Layout/LayoutDefault.jsx
+++ final-project-front-end/src/Layout/LayoutDefault.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 
-import Header from "../Components/header/DefaultHeader.jsx";
+import Header from "../Components/Header/DefaultHeader.jsx";
 
 export default function LayoutDefault() {
   return (
